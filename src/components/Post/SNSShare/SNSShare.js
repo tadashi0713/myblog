a325f82f21dfcb80b9ac33e276469a896ca075c0
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-02-03 23:30:04 +0900

    Add hatena

--- src/components/Post/SNSShare/SNSShare.js
+++ src/components/Post/SNSShare/SNSShare.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import {
   FacebookShareButton,
   FacebookIcon,
@@ -12,6 +13,10 @@ import {
 
 const SNSShare = ({ postUrl, postTitle }) => (
   <div>
+    <Helmet>
+      <script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async" />
+      <script type="text/javascript" src="//widgets.getpocket.com/v1/j/btn.js?v=1"  charset="utf-8" async="async" />
+    </Helmet>
     <FacebookShareButton url={postUrl}>
       <FacebookIcon size={32} round />
     </FacebookShareButton>
@@ -24,6 +29,20 @@ const SNSShare = ({ postUrl, postTitle }) => (
     <LineShareButton title={postTitle} url={postUrl}>
       <LineIcon size={32} round />
     </LineShareButton>
+    <a
+      aria-label="share_hatena-bookmark"
+      href="http://b.hatena.ne.jp/entry/"
+      className="hatena-bookmark-button"
+      data-hatena-bookmark-layout="touch-counter"
+      title="このエントリーをはてなブックマークに追加"
+      style={{boxShadow: 'none' }}>
+      <img
+        src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+        alt="このエントリーをはてなブックマークに追加"
+        width="24"
+        height="24"
+        style={{border: 'none'}} />
+    </a>
   </div>
 );
 
