80d89abb54ac5e95eae6c6437d23318e6150b7d5
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-02-03 23:49:49 +0900

    Add styles

--- src/components/Post/SNSShare/SNSShare.js
+++ src/components/Post/SNSShare/SNSShare.js
@@ -10,52 +10,65 @@ import {
   LineShareButton,
   LineIcon
 } from 'react-share';
+import styles from './SNSShare.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGetPocket } from '@fortawesome/free-brands-svg-icons';
 
 const SNSShare = ({ postUrl, postTitle }) => (
-  <div>
+  <ul className={styles.content}>
     <Helmet>
       <script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async" />
       <script type="text/javascript" src="//widgets.getpocket.com/v1/j/btn.js?v=1"  charset="utf-8" async="async" />
     </Helmet>
-    <FacebookShareButton url={postUrl}>
-      <FacebookIcon size={32} round />
-    </FacebookShareButton>
-    <TwitterShareButton title={postTitle} url={postUrl}>
-      <TwitterIcon size={32} round />
-    </TwitterShareButton>
-    <LinkedinShareButton url={postUrl}>
-      <LinkedinIcon size={32} round />
-    </LinkedinShareButton>
-    <LineShareButton title={postTitle} url={postUrl}>
-      <LineIcon size={32} round />
-    </LineShareButton>
-    <a
-      aria-label="share_pocket"
-      // className={styles.pocket_icon_link}
-      href={`https://getpocket.com/edit?url=${postUrl}&title=${postTitle}`}
-      onClick={`window.open(this.href, 'PCwindow', 'width=550, height=350, menubar=no, toolbar=no, scrollbars=yes'); return false;`}>
-      <FontAwesomeIcon
-        color="#fff"
-        // className={styles.pocket_icon}
-        icon={faGetPocket} />
-    </a>
-    <a
-      aria-label="share_hatena-bookmark"
-      href="http://b.hatena.ne.jp/entry/"
-      className="hatena-bookmark-button"
-      data-hatena-bookmark-layout="touch-counter"
-      title="このエントリーをはてなブックマークに追加"
-      style={{boxShadow: 'none' }}>
-      <img
-        src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
-        alt="このエントリーをはてなブックマークに追加"
-        width="24"
-        height="24"
-        style={{border: 'none'}} />
-    </a>
-  </div>
+    <li className={styles.share_button}>
+      <FacebookShareButton url={postUrl}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+    </li>
+    <li className={styles.share_button}>
+      <TwitterShareButton title={postTitle} url={postUrl}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+    </li>
+    <li className={styles.share_button}>
+      <LinkedinShareButton url={postUrl}>
+        <LinkedinIcon size={32} round />
+      </LinkedinShareButton>
+    </li>
+    <li className={styles.share_button}>
+      <LineShareButton title={postTitle} url={postUrl}>
+        <LineIcon size={32} round />
+      </LineShareButton>
+    </li>
+    <li className={styles.share_button}>
+      <a
+        aria-label="share_pocket"
+        className={styles.pocket_icon_link}
+        href={`https://getpocket.com/edit?url=${postUrl}&title=${postTitle}`}
+        onClick={`window.open(this.href, 'PCwindow', 'width=550, height=350, menubar=no, toolbar=no, scrollbars=yes'); return false;`}>
+        <FontAwesomeIcon
+          color="#fff"
+          className={styles.pocket_icon}
+          icon={faGetPocket} />
+      </a>
+    </li>
+    <li className={styles.share_button}>
+      <a
+        aria-label="share_hatena-bookmark"
+        href="http://b.hatena.ne.jp/entry/"
+        className="hatena-bookmark-button"
+        data-hatena-bookmark-layout="touch-counter"
+        title="このエントリーをはてなブックマークに追加"
+        style={{boxShadow: 'none' }}>
+        <img
+          src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+          alt="このエントリーをはてなブックマークに追加"
+          width="24"
+          height="24"
+          style={{border: 'none'}} />
+      </a>
+    </li>
+  </ul>
 );
 
 export default SNSShare;
