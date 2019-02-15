31706964e04a73dc02d0410cafd3c82ceb23945a
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-02-15 14:07:32 +0900

    Fix lint

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import styles from './Layout.module.scss';
 
-const Layout = ({ children, title, description, image }) => (
+const Layout = ({
+  children, title, description, image
+}) => (
   <div className={styles.layout}>
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="og:image" content={image}/>
+      <meta name="og:image" content={image} />
     </Helmet>
     {children}
   </div>
