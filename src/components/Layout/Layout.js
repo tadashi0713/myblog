1701457a206171634682bde9cccb4153dcdd6866
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-02-15 13:59:49 +0900

    Add ogp image tag

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import styles from './Layout.module.scss';
 
-const Layout = ({ children, title, description }) => (
+const Layout = ({ children, title, description, image }) => (
   <div className={styles.layout}>
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="og:image" content={image}/>
     </Helmet>
     {children}
   </div>
