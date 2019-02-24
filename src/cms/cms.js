f55290e11546d730d5eb6e7ef8d82ea147bb2157
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-02-24 19:09:00 +0900

    Fix lint

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms';
 
-import './style.css'
+import './style.css';
 
 import PagePreview from './preview-templates/page-preview';
 import PostPreview from './preview-templates/post-preview';
