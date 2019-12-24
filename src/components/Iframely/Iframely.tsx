073f636d07598f7ca9bd3c2c2b26ab40db09968e
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-12-24 20:59:26 +0900

    Ignore @ts-ignore

--- src/components/Iframely/Iframely.tsx
+++ src/components/Iframely/Iframely.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import Helmet from "react-helmet";
 
 export default class Iframely extends React.Component {
-  componentDidMount() {
+  static componentDidMount() {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore
     if (window.iframely) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
       // @ts-ignore
       window.iframely.load();
     }
   }
 
-  render() {
+  static render() {
     return (
       <Helmet>
         <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" />
