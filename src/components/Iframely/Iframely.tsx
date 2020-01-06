980eff8d8171fd2c49222a0c59cc8f0f7aa0806d
tadashi0713 <tadashi.nemoto0713@gmail.com>
2020-01-06 11:57:30 +0900

    Not use static

--- src/components/Iframely/Iframely.tsx
+++ src/components/Iframely/Iframely.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 
 export default class Iframely extends React.Component {
-  static componentDidMount() {
+  componentDidMount() {
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore
     if (window.iframely) {
@@ -12,7 +12,7 @@ export default class Iframely extends React.Component {
     }
   }
 
-  static render() {
+  render() {
     return (
       <Helmet>
         <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" />
