a6d37feb08070d3534008891c9d9c772fa10fef5
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2020-06-28 17:10:37 +0900

    use dayjs instead of momentjs

--- src/components/Post/Meta/Meta.tsx
+++ src/components/Post/Meta/Meta.tsx
@@ -1,11 +1,11 @@
 import React, { ReactElement } from "react";
-import moment from "moment";
+import dayjs from "dayjs";
 import styles from "./Meta.module.scss";
 
 const Meta = ({ date }): ReactElement => (
   <div className={styles.meta}>
     <p className={styles.meta__date}>
-      Published {moment(date).format("D MMM YYYY")}
+      Published {dayjs(date).format("D MMM YYYY")}
     </p>
   </div>
 );
