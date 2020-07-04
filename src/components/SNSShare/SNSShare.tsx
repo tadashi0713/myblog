9358425057017c4879d88fde4a6450f9c987f40e
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2020-07-04 16:04:16 +0900

    Change component

--- src/components/SNSShare/SNSShare.tsx
+++ src/components/SNSShare/SNSShare.tsx
@@ -1,57 +1,86 @@
-import React, { ReactElement } from "react";
-import {
-  EmailShareButton,
-  FacebookShareButton,
-  FacebookIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  LinkedinShareButton,
-  LinkedinIcon,
-  PocketShareButton,
-  LineShareButton,
-  LineIcon,
-  EmailIcon,
-  PocketIcon,
-} from "react-share";
-import { HatenabookmarkButton } from "react-social-sharebuttons";
-import styles from "./SNSShare.module.scss";
+import React, { useEffect } from "react";
 
-const SNSShare = ({ postUrl, postTitle }): ReactElement => (
-  <ul className={styles.content}>
-    <li className={styles.share_button}>
-      <EmailShareButton url={postUrl}>
-        <EmailIcon size={32} round />
-      </EmailShareButton>
-    </li>
-    <li className={styles.share_button}>
-      <FacebookShareButton url={postUrl}>
-        <FacebookIcon size={32} round />
-      </FacebookShareButton>
-    </li>
-    <li className={styles.share_button}>
-      <TwitterShareButton title={postTitle} url={postUrl}>
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
-    </li>
-    <li className={styles.share_button}>
-      <LinkedinShareButton url={postUrl}>
-        <LinkedinIcon size={32} round />
-      </LinkedinShareButton>
-    </li>
-    <li className={styles.share_button}>
-      <LineShareButton title={postTitle} url={postUrl}>
-        <LineIcon size={32} round />
-      </LineShareButton>
-    </li>
-    <li className={styles.share_button}>
-      <PocketShareButton title={postTitle} url={postUrl}>
-        <PocketIcon size={32} round />
-      </PocketShareButton>
-    </li>
-    <li className={styles.share_button}>
-      <HatenabookmarkButton url={postUrl} layout="touch-counter" />
-    </li>
-  </ul>
-);
+type Props = {
+  title: string;
+  url: string;
+};
 
-export default SNSShare;
+interface WindowWithTwitter extends Window {
+  twttr?: {
+    widgets?: {
+      load: (current?: HTMLElement) => void;
+    };
+  };
+
+  FB?: {
+    XFBML?: {
+      parse: (current?: HTMLElement) => void;
+    };
+  };
+}
+
+declare let window: WindowWithTwitter;
+
+const SharingButtons: React.FC<Props> = ({ title, url }: Props) => {
+  useEffect(() => {
+    // Gatsby は React コンポーネントを静的な HTML にビルドするのに Node をランタイムとして利用する。
+    // このため window オブジェクトがないと怒られるので、ビルド時には window を利用する箇所をスルーさせる。
+    if (typeof window !== `undefined`) {
+      if (window.FB !== `undefined`) {
+        window.FB.XFBML.parse();
+      }
+
+      if (window.twttr !== `undefined`) {
+        window.twttr.widgets.load();
+      }
+    }
+  }, []);
+
+  return (
+    <div className="sns-wrapper">
+      {/* Facebook */}
+      <li>
+        <div
+          className="fb-like"
+          data-href={url}
+          data-width=""
+          data-layout="button"
+          data-action="like"
+          data-size="small"
+          data-share="false"
+        />
+      </li>
+      {/* Twitter */}
+      <li>
+        <a
+          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+          className="twitter-share-button"
+          data-text={title}
+          data-show-count="false"
+        >
+          Tweet
+        </a>
+      </li>
+      {/* hatebu */}
+      <li>
+        <a
+          href="https://b.hatena.ne.jp/entry/"
+          className="hatena-bookmark-button"
+          data-hatena-bookmark-layout="basic-label"
+          data-hatena-bookmark-lang="en"
+          data-hatena-bookmark-height="20"
+          title="このエントリーをはてなブックマークに追加"
+        >
+          <img
+            src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png"
+            alt="このエントリーをはてなブックマークに追加"
+            width="20"
+            height="20"
+            style={{ border: "none" }}
+          />
+        </a>
+      </li>
+    </div>
+  );
+};
+export default SharingButtons;
