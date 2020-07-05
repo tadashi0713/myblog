dd662285c2f9b4478e54808fc1c0a02803e74890
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2020-07-05 13:25:12 +0900

    gatsby-plugin-no-javascript

--- gatsby-config.js
+++ gatsby-config.js
@@ -256,5 +256,6 @@ module.exports = {
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
+    "gatsby-plugin-no-javascript",
   ],
 };
