05cc8caf24ec5a9964e16ad7a917d69b964a7f11
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2020-07-05 13:03:44 +0900

    Solve type problem

--- src/components/Iframely/Iframely.tsx
+++ src/components/Iframely/Iframely.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
-export default class Iframely extends React.Component {
-  componentDidMount() {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
+interface WindowWithIframely extends Window {
+  iframely?: {
+    load: (current?: HTMLElement) => void;
+  };
+}
+
+declare let window: WindowWithIframely;
+
+const Iframely: React.FC = () => {
+  useEffect(() => {
+    // Gatsby は React コンポーネントを静的な HTML にビルドするのに Node をランタイムとして利用する。
+    // このため window オブジェクトがないと怒られるので、ビルド時には window を利用する箇所をスルーさせる。
     if (window.iframely) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       window.iframely.load();
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <Helmet>
-        <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" />
-      </Helmet>
-    );
-  }
-}
+  return (
+    <Helmet>
+      <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" />
+    </Helmet>
+  );
+};
+
+export default Iframely;
